9a21d24bb0d561bcc115ad5299d14e26243207b8
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2017-01-23 11:07:49 +0100

    Asks for branch

--- deploy/config.rb
+++ deploy/config.rb
@@ -5,7 +5,7 @@ set :application, 'app_demo'
 set :repo_url, 'git@github.com:mjacobus/capistrano-demo.git'
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/var/www/apps/app_demo'
